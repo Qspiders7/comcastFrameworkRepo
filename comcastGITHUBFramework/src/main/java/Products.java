bbb02f37ee4dba95c065fa0b349343f3d4c38de6
priya <priya@LAPTOP-CE7R4UV6>
2025-02-27 18:22:39 +0530

    Product , Added in POM class

---
+++ comcastGITHUBFramework/src/main/java/Products.java
@@ -0,0 +1,8 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+
+	@FindBy(xpath="//input[@alt='Create Product...']")
+	private WebElement createProducyImgBtn;
+}
